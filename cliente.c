22a086c3631cec545fc24f55ba056c502a57ead6
kevinvarela <kevin.varela@despegar.com>
2025-05-06 22:47:44 -0300

    refactor naming var

--- cliente.c
+++ cliente.c
@@ -5,13 +5,13 @@
 #include <sys/socket.h>
 
 int main(void) {
-	struct sockaddr_in direcciónServidor;
-	direcciónServidor.sin_family = AF_INET;
-	direcciónServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
-	direcciónServidor.sin_port = htons(8080);
+	struct sockaddr_in dirServidor;
+	dirServidor.sin_family = AF_INET;
+	dirServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+	dirServidor.sin_port = htons(8080);
 
 	int cliente = socket(AF_INET, SOCK_STREAM, 0);
-	if (connect(cliente, (void*) &direcciónServidor, sizeof(direcciónServidor)) != 0) {
+	if (connect(cliente, (void*) &dirServidor, sizeof(dirServidor)) != 0) {
 		perror("No se pudo conectar");
 		return 1;
 	}
--- servidor.c
+++ servidor.c
@@ -5,17 +5,17 @@
 #include <sys/socket.h>
 
 int main(void) {
-	struct sockaddr_in direcciónServidor;
-	direcciónServidor.sin_family = AF_INET;
-	direcciónServidor.sin_addr.s_addr = INADDR_ANY;
-	direcciónServidor.sin_port = htons(8080);
+	struct sockaddr_in dirServidor;
+	dirServidor.sin_family = AF_INET;
+	dirServidor.sin_addr.s_addr = INADDR_ANY;
+	dirServidor.sin_port = htons(8080);
 
 	int servidor = socket(AF_INET, SOCK_STREAM, 0);
 
 	int activado = 1;
 	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
 
-	if (bind(servidor, (void*) &direcciónServidor, sizeof(direcciónServidor)) != 0) {
+	if (bind(servidor, (void*) &dirServidor, sizeof(dirServidor)) != 0) {
 		perror("Falló el bind");
 		return 1;
 	}
@@ -25,9 +25,9 @@ int main(void) {
 
 	//------------------------------
 
-	struct sockaddr_in direcciónCliente;
-	unsigned int tamañoDirección;
-	int cliente = accept(servidor, (void*) &direcciónCliente, &tamañoDirección);
+	struct sockaddr_in dirCliente;
+	unsigned int tamDir;
+	int cliente = accept(servidor, (void*) &dirCliente, &tamDir);
 
 	printf("Recibí una conexión en %d!!\n", cliente);
 	send(cliente, "Hola NetCat!", 13, 0);
